e87d23d12a26466cb53e328f229ca28f371ba405
ejgaug <157926449+ejgaug@users.noreply.github.com>
2024-01-29 11:44:55 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ASSET_URL = 'https://ejgaug.github.io/FantasyFootballForecaster/' || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `${ASSET_URL}/dist/`,
+  [...]
 })
