f87237e00a539a88cd2c216ae7caa9327968d0d3
EGaug <144638221+EGaug@users.noreply.github.com>
2024-01-29 12:24:44 -0600

    auto redirect for nomatch

--- src/components/nav/pages/FantasyNoMatch.jsx
+++ src/components/nav/pages/FantasyNoMatch.jsx
@@ -2,14 +2,5 @@ import { Link } from "react-router-dom";
 
 export default function FantasyNoMatch(props) {
 
-    return (
-        <Redirect to="/" />
-    //     <div>
-    //         <h2>That's a 404.</h2>
-    //         <p>Uh oh, looks like you've taken a wrong turn!</p>
-    //         <p>
-    //             <Link to="/">Back to safety.</Link>
-    //         </p>
-    //     </div>
-    );
+    return <Redirect to="/" />;
 }
