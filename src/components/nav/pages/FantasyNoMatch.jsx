c739d9eeeb98f8ecec812c4b46157e3b42706bd5
EGaug <144638221+EGaug@users.noreply.github.com>
2024-01-29 12:21:18 -0600

    automatic redirect back to home

--- src/components/nav/pages/FantasyNoMatch.jsx
+++ src/components/nav/pages/FantasyNoMatch.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 export default function FantasyNoMatch(props) {
 
     return (
-        <div>
-            <h2>That's a 404.</h2>
-            <p>Uh oh, looks like you've taken a wrong turn!</p>
-            <p>
-                <Link to="/">Back to safety.</Link>
-            </p>
-        </div>
+        <Redirect to="/" />
+    //     <div>
+    //         <h2>That's a 404.</h2>
+    //         <p>Uh oh, looks like you've taken a wrong turn!</p>
+    //         <p>
+    //             <Link to="/">Back to safety.</Link>
+    //         </p>
+    //     </div>
     );
 }
